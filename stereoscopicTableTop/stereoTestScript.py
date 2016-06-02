1447b9c3d8acb38eb5dca5c43c1975f9458cbb9b
bmj8778 <gabriel.diaz@rit.edu>
2016-06-02 17:57:54 -0400

    Added test script.

---
+++ stereoscopicTableTop/stereoTestScript.py
@@ -0,0 +1,12 @@
+﻿#Test file for displaying sterescopic scene using the rear projection setup.
+
+import viz
+
+viz.window.setFullscreenMonitor(2)
+viz.go(viz.QUAD_BUFFER)
+
+viz.window.setFullscreen(viz.ON)
+
+#viz.MainWindow.setStereoSwap(viz.TOGGLE)
+
+piazza = viz.addChild('piazza.osgb')
