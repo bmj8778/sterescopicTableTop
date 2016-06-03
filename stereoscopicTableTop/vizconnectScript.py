11d701c1099312bf130d57957f58b74c8bbaad54
bmj8778 <gabriel.diaz@rit.edu>
2016-06-03 14:41:21 -0400

    Added vizconnect example. Loads a generic tracker, but does not actual tracking yet.

---
+++ stereoscopicTableTop/vizconnectScript.py
@@ -0,0 +1,6 @@
+﻿vrpn = viz.add('vrpn7.dle') 
+
+#uses name from the vrpn.cfg file where vrpn server runs.
+tracker1 = vrpn.addTracker('Tracker0@localhost') #Default sensor is 0 
+
+#tracker2 = vrpn.addTracker('Tracker0@localhost',1)
